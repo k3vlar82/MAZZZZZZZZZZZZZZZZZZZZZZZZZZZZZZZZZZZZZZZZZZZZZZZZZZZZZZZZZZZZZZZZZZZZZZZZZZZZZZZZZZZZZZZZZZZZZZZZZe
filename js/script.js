b9a01f985b9e062a327518a5258347d6c603b8fe
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-04-12 11:51:21 -0600

    formatting and things

--- js/script.js
+++ js/script.js
@@ -16,25 +16,28 @@ window.onload = function(){
     //set neighbor locations with a function and if statements
     //LOTS OF DRAWING
     // DRAW ALL THE CODE    
-        const SIZE = 4;
-         const CELL_SIZE = 69;
+        const SIZE = 2;
+        const CELL_SIZE = 69;
+        const LEFT = x--;
+        const RIGHT = x++;
+        const DOWN = y++;
+        const UP = y--;
     
-         var myGrid = [];
+        var myGrid = [];
 
         var selectedX = 0;
         var selectedY = 0;
 
-         
     
-         for(var y = 0; y < SIZE; y++){
+        for(var y = 0; y < SIZE; y++){
             myGrid[y] = [];
         
             for(var x = 0; x < SIZE; x++){
                 myGrid[y][x] = y + "," + x;
             }
-         }
+        }
     
-         console.log(myGrid);
+        console.log(myGrid);
 
             // if(selectedX == x && selectedY == y){
             //     newCell.style.border = ".5px solid red";
@@ -42,29 +45,29 @@ window.onload = function(){
 
             redrawGrid();
             
-         function redrawGrid(){
-         var gridDiv = document.getElementById("grid");
+        function redrawGrid(){
+            var gridDiv = document.getElementById("grid");
     
          for(var y = 0; y < SIZE; y++){
-             var newRow = document.createElement("div");
+                var newRow = document.createElement("div");
     
-             for(var x = 0; x < SIZE; x++){
-                var newCell = document.createElement("div");
+                for(var x = 0; x < SIZE; x++){
+                      var newCell = document.createElement("div");
     
-                newCell.style.height = CELL_SIZE + "px";
-                newCell.style.width = CELL_SIZE + "px";
-                newCell.style.backgroundColor = "#abcdef";
-                newCell.style.color = "#123456";
-                newCell.style.display = "inline-block";
-                newCell.style.border = "solid .5px black";
-                newCell.style.textAlign = "center";
+                      newCell.style.height = CELL_SIZE + "px";
+                      newCell.style.width = CELL_SIZE + "px";
+                      newCell.style.backgroundColor = "#abcdef";
+                      newCell.style.color = "#123456";
+                      newCell.style.display = "inline-block";
+                    newCell.style.border = "solid .5px black";
+                    ewCell.style.textAlign = "center";
     
                 newCell.textContent = y + "," + x;
     
                 newRow.appendChild(newCell);
-             }
+                }
     
-             gridDiv.appendChild(newRow);
-         }
+                gridDiv.appendChild(newRow);
+            }
         }
     }
