d445910b4d59d5d43b5de152050b64cbf3dd2622
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-04-13 11:49:03 -0600

    things

--- js/script.js
+++ js/script.js
@@ -1,1 +1,31 @@
-((Math.floor(Math.random((SIZE-1)/2)))*2)+1;
+window.onload = function () {
+    const SIZE = 2;
+    const CELL_SIZE = 69;
+
+    ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
+    redrawGrid();
+    function redrawGrid() {
+        var gridDiv = document.getElementById("grid");
+
+        for (var y = 0; y < SIZE; y++) {
+            var newRow = document.createElement("div");
+
+            for (var x = 0; x < SIZE; x++) {
+                var newCell = document.createElement("div");
+
+                newCell.style.height = CELL_SIZE + "px";
+                newCell.style.width = CELL_SIZE + "px";
+                newCell.style.backgroundColor = "#abcdef";
+                newCell.style.color = "#123456";
+                newCell.style.display = "inline-block";
+                newCell.style.border = "solid .5px black";
+                newCell.style.textAlign = "center";
+                newCell.textContent = y + "," + x;
+
+                newRow.appendChild(newCell);
+            }
+
+            gridDiv.appendChild(newRow);
+        }
+    }
+}
