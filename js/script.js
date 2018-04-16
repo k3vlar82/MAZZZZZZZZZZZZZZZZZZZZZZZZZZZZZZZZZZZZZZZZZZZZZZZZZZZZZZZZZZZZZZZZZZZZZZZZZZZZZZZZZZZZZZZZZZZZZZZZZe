ddb20d585565e4cfbd56e5870baaccbc50a7749e
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-04-16 12:05:39 -0600

    k

--- js/script.js
+++ js/script.js
@@ -18,32 +18,29 @@ window.onload = function () {
 
     ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
     function redrawGrid() {
-        // var gridDiv = document.getElementById("grid");
+        var gridDiv = document.getElementById("grid");
         
-        // for (var y = 0; y < SIZE; y++) {
-        //     var newRow = document.createElement("div");
+        for (var y = 0; y < SIZE; y++) {
+            var newRow = document.createElement("div");
             
-        //     for (var x = 0; x < SIZE; x++) {
-        //         var newCell = document.createElement("div");
+            for (var x = 0; x < SIZE; x++) {
+                var newCell = document.createElement("div");
                 
-        //         newCell.style.height = CELL_SIZE + "px";
-        //         newCell.style.width = CELL_SIZE + "px";
-        //         newCell.style.backgroundColor = "#abcdef";
-        //         newCell.style.color = "#123456";
-        //         newCell.style.display = "inline-block";
-        //         newCell.style.bordert = "solid .5px black";
-        //         newCell.style.textAlign = "center";
-        //         newCell.textContent = y + "," + x;
+                newCell.style.height = CELL_SIZE + "px";
+                newCell.style.width = CELL_SIZE + "px";
+                newCell.style.backgroundColor = "#abcdef";
+                newCell.style.color = "#123456";
+                newCell.style.display = "inline-block";
+                newCell.style.border = "solid .5px black";
+                newCell.style.textAlign = "center";
+                newCell.textContent = y + "," + x;
 
-        //         newRow.appendChild(newCell);
-        //     }
+                newRow.appendChild(newCell);
+            }
             
-        //     gridDiv.appendChild(newRow);
-        // }
-        // console.log(gridDiv);
-
-        const SIZE = 4;
-     const CELL_SIZE = 69;
+            gridDiv.appendChild(newRow);
+        }
+        console.log(gridDiv);
 
      var myGrid = [];
 
