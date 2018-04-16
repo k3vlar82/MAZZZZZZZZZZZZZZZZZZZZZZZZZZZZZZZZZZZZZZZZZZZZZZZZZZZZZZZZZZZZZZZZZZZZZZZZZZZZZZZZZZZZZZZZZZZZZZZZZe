3a78eabd2cbf1bdf35cbc4692d0eb1d31cfe3e6c
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-04-16 11:18:27 -0600

    DFS algo

--- js/script.js
+++ js/script.js
@@ -41,4 +41,48 @@ window.onload = function () {
         }
 
     }
+
+    function dfs(maze, stack)
+    {
+        var cell = stack.peek();
+    
+        x = cell.x;
+        y = cell.y;
+        neighbors = cell.neighbors;
+    
+        // when all cells have been visited at least once, it's done
+        if(maze.cells[x][y].visited == false)
+        {
+            maze.cells[x][y].visited = true;
+            maze.visitedCount++;
+        }
+    
+        /* look for a neighbor that is in the maze and hasn't been visited yet */
+        while(neighbors.length > 0)
+        {
+            dir = neighbors.pop();
+            if(neighbors.length == 0)
+            {
+                stack.pop(); // all neighbors checked, done with this one.
+            }
+    
+            dx = x + delta.x[dir];
+            dy = y + delta.y[dir];
+            if(dx >= 0 && dy >= 0 && dx < columns && dy < rows)
+            {
+                if(maze.cells[dx][dy].visited == false)
+                {
+                    /* break down the wall between them. The new neighbor */
+                    /* is pushed onto the stack and becomes the new current cell */
+                    maze.cells[x][y].wall[dir] = false;
+                    maze.cells[dx][dy].wall[undirs[dir]] = false;
+                    stack.push( { x: dx, y: dy, neighbors: dirs.shuffle() } );
+                    break;
+                }
+            }
+        }
+        
+        return maze.visitedCount; // when visited count == row * columns it's done
+    }
+
 }
