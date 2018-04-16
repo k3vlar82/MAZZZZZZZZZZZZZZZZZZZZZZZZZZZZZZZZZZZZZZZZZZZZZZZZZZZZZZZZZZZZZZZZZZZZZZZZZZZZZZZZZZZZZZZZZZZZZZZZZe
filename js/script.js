d6b83da938f1f170c21d95ba76a54cca54e42f2f
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-04-16 12:38:06 -0600

    cell object

--- js/script.js
+++ js/script.js
@@ -1,30 +1,46 @@
 window.onload = function () {
 
-
     var rows = 3;
     var columns = 3;
     var box;
     var instance;
     var side;
-    
+
     var above = 0;
     var below = 1;
-    var left  = 2;
+    var left = 2;
     var right = 3;
-    
-    var dirs   = [0,1,2,3]; // direction of neighbors
-    var undirs = [1,0,3,2]; // opposite direction
-    var delta  = { x:[0, 0,-1, 1], y:[-1, 1, 0, 0] }; // offsets of neighbors
-    
+
+    function Cell(x, y) {
+        this.walls = ['l', 'r', 'u', 'd'],
+            this.x = x;
+        this.y = y;
+        this.visited = false;
+        this.neighborsX = [x - 1, x, x + 1, x];
+        this.neighborsY = [y, y + 1, y, y - 1];
+    }
+
+    function neighbor() {
+
+    }
 
     const SIZE = 2;
     const CELL_SIZE = 69;
     var node = '';
-    // var cell = {
-    // };
+
+    function maze() {
+        this.rows;
+        this.columns;
+        this.SIZE;
+        this.CELL_SIZE;
+        this.neighbors;
+        this.side;
+
+    }
 
     ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
     redrawGrid();
+
     function redrawGrid(maze, stack) {
         var gridDiv = document.getElementById("grid");
 
@@ -32,53 +48,13 @@ window.onload = function () {
             var newRow = document.createElement("div");
 
             for (var x = 0; x < SIZE; x++) {
-                var newCell = document.createElement("div");
+                var newCell = new Cell[x][y];
 
 
 
+                side.remove();
+                return maze.visitedCount;
 
-                function dfs(maze, stack)
-                {
-                    var cell = stack.peek();
-                
-                    x = cell.x;
-                    y = cell.y;
-                    neighbors = cell.neighbors;
-                
-                    // when all cells have been visited at least once, it's done
-                    if(maze.cells[x][y].visited == false)
-                    {
-                        maze.cells[x][y].visited = true;
-                        maze.visitedCount++;
-                    }
-                
-                    /* look for a neighbor that is in the maze and hasn't been visited yet */
-                    while(neighbors.length > 0)
-                    {
-                        dir = neighbors.pop();
-                        if(neighbors.length == 0)
-                        {
-                            stack.pop(); // all neighbors checked, done with this one.
-                        }
-                
-                        dx = x + delta.x[dir];
-                        dy = y + delta.y[dir];
-                        if(dx >= 0 && dy >= 0 && dx < columns && dy < rows)
-                        {
-                            if(maze.cells[dx][dy].visited == false)
-                            {
-                                /* break down the wall between them. The new neighbor */
-                                /* is pushed onto the stack and becomes the new current cell */
-                                maze.cells[x][y].wall[dir] = false;
-                                maze.cells[dx][dy].wall[undirs[dir]] = false;
-                                stack.push( { x: dx, y: dy, neighbors: dirs.shuffle() } );
-                                break;
-                            }
-                        }
-                    }
-                    
-                    return maze.visitedCount; // when visited count == row * columns it's done
-                
 
                 newCell.style.height = CELL_SIZE + "px";
                 newCell.style.width = CELL_SIZE + "px";
@@ -94,31 +70,23 @@ window.onload = function () {
 
             gridDiv.appendChild(newRow);
         }
-       
+
     }
 
-   function maze(){
-    this.rows;
-    this.columns;
-    this.SIZE;
-    this.CELL_SIZE;
-    this.neighbors;
 
-   }
-
-   function remove(){
-    if(side == 0){
-    newCell.style.borderTop = "solid .5px #abcdef";
-    } if (side == 1){
-        newCell.style.borderBottom = "solid .5px #abcdef";
-    } if (side == 2){
-        newCell.style.borderLeft = "solid .5px #abcdef";
-    } if (side == 3) {
-        newCell.style.borderRight = "solid .5px #abcdef";
+    function remove() {
+        if (side == 0) {
+            newCell.style.borderTop = "solid .5px #abcdef";
+        } if (side == 1) {
+            newCell.style.borderBottom = "solid .5px #abcdef";
+        } if (side == 2) {
+            newCell.style.borderLeft = "solid .5px #abcdef";
+        } if (side == 3) {
+            newCell.style.borderRight = "solid .5px #abcdef";
+        }
     }
-   }
 
-   
+
 }
 
 
