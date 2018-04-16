bcb2b6e33d03c4e9c9df6060ffcd7f2d588b2c11
Kevin <32335809+k3vlar82@users.noreply.github.com>
2018-04-16 12:04:38 -0600

    starting over... again

--- js/script.js
+++ js/script.js
@@ -1,88 +1,73 @@
 window.onload = function () {
-    const SIZE = 2;
+    const SIZE = 4;
     const CELL_SIZE = 69;
     var node = '';
+    var rows;
+    var columns;
+    var box;
+    var instance;
+
+    var above = 0;
+    var below = 1;
+    var left = 2;
+    var right = 3;
+
+    var dirs = [0, 1, 2, 3]; // direction of neighbors
+    var undirs = [1, 0, 3, 2]; // opposite direction
+    var delta = { x: [0, 0, -1, 1], y: [-1, 1, 0, 0] }; // offsets of neighbors
 
     ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
-    redrawGrid();
     function redrawGrid() {
-        var gridDiv = document.getElementById("grid");
+        // var gridDiv = document.getElementById("grid");
+        
+        // for (var y = 0; y < SIZE; y++) {
+        //     var newRow = document.createElement("div");
+            
+        //     for (var x = 0; x < SIZE; x++) {
+        //         var newCell = document.createElement("div");
+                
+        //         newCell.style.height = CELL_SIZE + "px";
+        //         newCell.style.width = CELL_SIZE + "px";
+        //         newCell.style.backgroundColor = "#abcdef";
+        //         newCell.style.color = "#123456";
+        //         newCell.style.display = "inline-block";
+        //         newCell.style.bordert = "solid .5px black";
+        //         newCell.style.textAlign = "center";
+        //         newCell.textContent = y + "," + x;
 
-        for (var y = 0; y < SIZE; y++) {
-            var newRow = document.createElement("div");
+        //         newRow.appendChild(newCell);
+        //     }
+            
+        //     gridDiv.appendChild(newRow);
+        // }
+        // console.log(gridDiv);
 
-            for (var x = 0; x < SIZE; x++) {
-                var newCell = document.createElement("div");
+        const SIZE = 4;
+     const CELL_SIZE = 69;
 
-                newCell.style.height = CELL_SIZE + "px";
-                newCell.style.width = CELL_SIZE + "px";
-                newCell.style.backgroundColor = "#abcdef";
-                newCell.style.color = "#123456";
-                newCell.style.display = "inline-block";
-                newCell.style.border = "solid .5px black";
-                newCell.style.textAlign = "center";
-                newCell.textContent = y + "," + x;
+     var myGrid = [];
 
-                newRow.appendChild(newCell);
-            }
-
-            gridDiv.appendChild(newRow);
+     for(var y = 0; y < SIZE; y++){
+        myGrid[y] = [];
+    
+        for(var x = 0; x < SIZE; x++){
+            myGrid[y][x] = y + "," + x;
         }
-        if(SIZE % 2 == 0){
+     }
+     console.log(myGrid);
+        
+        if (SIZE % 2 == 0) {
             //even numbers
-            if(x % 2 == 1 && y % 2 == 1){
+            if (x % 2 == 1 && y % 2 == 1) {
                 [x][y] = node;
             }
-        } else if (SIZE % 2 == 1){
+        } else if (SIZE % 2 == 1) {
             //odd numbers
-            if(x % 2 == 1 && y % 2 == 1){
+            if (x % 2 == 1 && y % 2 == 1) {
                 [x][y] = node;
-            }
-        }
-
-    }
-
-    function dfs(maze, stack)
-    {
-        var cell = stack.peek();
-    
-        x = cell.x;
-        y = cell.y;
-        neighbors = cell.neighbors;
-    
-        // when all cells have been visited at least once, it's done
-        if(maze.cells[x][y].visited == false)
-        {
-            maze.cells[x][y].visited = true;
-            maze.visitedCount++;
-        }
-    
-        /* look for a neighbor that is in the maze and hasn't been visited yet */
-        while(neighbors.length > 0)
-        {
-            dir = neighbors.pop();
-            if(neighbors.length == 0)
-            {
-                stack.pop(); // all neighbors checked, done with this one.
-            }
-    
-            dx = x + delta.x[dir];
-            dy = y + delta.y[dir];
-            if(dx >= 0 && dy >= 0 && dx < columns && dy < rows)
-            {
-                if(maze.cells[dx][dy].visited == false)
-                {
-                    /* break down the wall between them. The new neighbor */
-                    /* is pushed onto the stack and becomes the new current cell */
-                    maze.cells[x][y].wall[dir] = false;
-                    maze.cells[dx][dy].wall[undirs[dir]] = false;
-                    stack.push( { x: dx, y: dy, neighbors: dirs.shuffle() } );
-                    break;
-                }
             }
         }
         
-        return maze.visitedCount; // when visited count == row * columns it's done
     }
-
+    redrawGrid();
 }
