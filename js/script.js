ec752ed8f0437bbfa0360399fde027ff41b915d3
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-04-16 17:36:57 -0600

    maze progress

--- js/script.js
+++ js/script.js
@@ -1,20 +1,15 @@
 window.onload = function () {
 
-    const SIZE = 3;
+    const SIZE = 6;
     const CELL_SIZE = 69;
     var rows;
     var columns;
     var box;
     var instance;
 
-    var above = 0;
-    var below = 1;
-    var left = 2;
-    var right = 3;
+    var myGrid = [];
 
-    var dirs = [0, 1, 2, 3]; // direction of neighbors
-    var undirs = [1, 0, 3, 2]; // opposite direction
-    var delta = { x: [0, 0, -1, 1], y: [-1, 1, 0, 0] }; // offsets of neighbors
+    var back = [start];
 
     function Cell(x, y) {
         this.walls = ['l', 'r', 'u', 'd'];
@@ -24,24 +19,56 @@ window.onload = function () {
         this.neighborsX = [x - 1, x, x + 1, x];
         this.neighborsY = [y, y + 1, y, y - 1];
     }
+    var visited = false;
+    redrawGrid();
 
-    function explore(x, y, grid) {
-        cell = grid[y][y];
-        cell.visited = true;
-        array.forEach(neighbor => {
-            neighbor = grid[y][x]
+    var start = (getRandomInt(SIZE), getRandomInt(SIZE), myGrid);
+
+
+    if (!visited) {
+        explore(getRandomInt(SIZE), getRandomInt(SIZE), myGrid);
+        visited = true;
+    }
+
+    console.log(start);
+    function explore(x, y, myGrid) {
+        var go;
+
+        console.log(myGrid);
+        cell = myGrid[y][x];
+        while (!visited) {
             if (!visited) {
-                explore(Cell.neighborsX, Cell.neighborsY, grid);
+                var direction = Math.floor(Math.random() * 3);
+                console.log(direction);
+                console.log(myGrid[y][x]);
+                if (direction == 0) {
+                    go = myGrid[x - 1][y];
+                    back.push();
+                } else if (direction == 1) {
+                    go = myGrid[x + 1][y];
+                    back.push();
+                } else if (direction == 2) {
+                    go = myGrid[x][y + 1];
+                    back.push();
+                } else if (direction == 3) {
+                    go = myGrid[x][y - 1];
+                    back.push();
+                }
+                cell.visited = true;
+                console.log(go);
+                explore(go.x, go.y, myGrid);
             }
+        }
+        back.pop();
 
-        });
+
+
     };
 
     ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
     function redrawGrid() {
 
         var gridDiv = document.getElementById("grid");
-
         for (var y = 0; y < SIZE; y++) {
             var newRow = document.createElement("div");
 
@@ -59,12 +86,11 @@ window.onload = function () {
 
                 newRow.appendChild(newCell);
             }
-
             gridDiv.appendChild(newRow);
         }
         console.log(gridDiv);
 
-        var myGrid = [];
+
 
         for (var y = 0; y < SIZE; y++) {
             myGrid[y] = [];
@@ -73,26 +99,14 @@ window.onload = function () {
                 myGrid[y][x] = new Cell(x, y);
             }
         }
-        console.log(myGrid);
-
-        function getRandomInt(max) {
-            return Math.floor(Math.random() * Math.floor(max));
-        }
-
-        // if (SIZE % 2 == 0) {
-        //     //even numbers
-        //     if (x % 2 == 1 && y % 2 == 1) {
-        //         [x][y] = node;
-        //     }
-        // } else if (SIZE % 2 == 1) {
-        //     //odd numbers
-        //     if (x % 2 == 1 && y % 2 == 1) {
-        //         [x][y] = node;
-        //     }
-        // }
+        //  console.log(myGrid);
 
     }
-    redrawGrid();
+
+    function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
 
 
 }
