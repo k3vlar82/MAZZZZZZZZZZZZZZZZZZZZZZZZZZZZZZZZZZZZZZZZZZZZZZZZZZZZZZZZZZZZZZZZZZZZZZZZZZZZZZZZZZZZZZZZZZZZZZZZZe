76f3254611b035949d772034c38d18c0da56b419
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-04-17 16:59:30 -0600

    more mess

--- js/script.js
+++ js/script.js
@@ -5,8 +5,6 @@ window.onload = function () {
 
     var myGrid = [];
 
-
-
     function Cell(x, y) {
         this.walls = ['l', 'r', 'u', 'd'];
         this.x = x;
@@ -29,80 +27,80 @@ window.onload = function () {
 
     var start = new Start();
 
-    console.log(start.x);
+    console.log(start.x, start.y);
 
-    var back = [start];
+    var back = [];
 
     // if (!visited) {
-        explore(start.x, start.y, myGrid);
+    explore(start.x, start.y, myGrid);
     //     visited = true;
     // }
 
 
     function explore(x, y, myGrid) {
-        var go = new Cell();
+        var go = myGrid[x][y];
 
         //console.log(myGrid);
         var cell = myGrid[x][y];
         cell.visited = true;
 
         // If I can go left
-        // go = left
-        // If !left.visisted
-        // explore(left.x,left.y, myGrid)
+            // go = left
+            // If !left.visisted
+                 // explore(left.x,left.y, myGrid)
         // If can go right
         // fi can go up
         // if can go down
 
-        //var direction = Math.floor(Math.random() * 4);
-        console.log(myGrid[y][x]);
-        if (x - 1 >= 0) {
-            cell.visited = true;
+        var direction = Math.floor(Math.random() * 4);
+        console.log(myGrid[x][y]);
+        if (direction == 0 && x - 1 >= 0) {
             go = myGrid[x - 1][y];
             if (!go.visited) {
                 go.visited = true;
+                back.push(go);
                 explore(go.x, go.y, myGrid);
             }
-            else if(go.visited){
+            else if (go.visited) {
                 explore(cell.x, cell.y, myGrid);
+               // back.pop();
             }
-            back.push();
-        } else if (x + 1 <= SIZE) {
-            cell.visited = true;
+        } else if (direction == 1 && x + 1 < SIZE) {
             go = myGrid[x + 1][y];
             if (!go.visited) {
                 go.visited = true;
+                back.push(go);
                 explore(go.x, go.y, myGrid);
             }
-            else if(go.visited){
+            else if (go.visited) {
+              //  back.pop();
                 explore(cell.x, cell.y, myGrid);
             }
-            back.push();
-        } else if (y - 1 >= 0) {
-            cell.visited = true;
-            go = myGrid[x][y - 1];
-            if (!go.visited) {
-                go.visited = true;
-                explore(go.x, go.y, myGrid);
-            }
-            else if(go.visited){
-                explore(cell.x, cell.y, myGrid);
-            }
-            back.push();
-        } else if (y + 1 <= SIZE) {
-            cell.visited = true;
+        } else if (direction == 2 && y + 1 < SIZE) {
             go = myGrid[x][y + 1];
             if (!go.visited) {
                 go.visited = true;
+                back.push(go);
                 explore(go.x, go.y, myGrid);
             }
-            else if(go.visited){
+            else if (go.visited) {
+             //   back.pop();
                 explore(cell.x, cell.y, myGrid);
             }
-            back.push();
+        } else if (direction == 3 && y - 1 >= 0) { // y is 0 - goes to this, pops last cell, goes back to this
+            go = myGrid[x][y - 1];
+            if (!go.visited) {
+                go.visited = true;
+                back.push(go);
+                explore(go.x, go.y, myGrid);
+            }
+            else if (go.visited) {
+              //  back.pop();
+                explore(cell.x, cell.y, myGrid);
+            }
         }
-        back.pop();
 
+        console.log(back);
         // if (!visited) {
         //     explore(go.x, go.y, myGrid);
         // }
@@ -119,7 +117,14 @@ window.onload = function () {
 
     ((Math.floor(Math.random((SIZE - 1) / 2))) * 2) + 1;
     function redrawGrid() {
+        for (var x = 0; x < SIZE; x++) {
+            myGrid[x] = [];
 
+            for (var y = 0; y < SIZE; y++) {
+                myGrid[x][y] = new Cell(x, y);
+
+            }
+        }
         // var gridDiv = document.getElementById("grid");
         // for (var y = 0; y < SIZE; y++) {
         //     var newRow = document.createElement("div");
@@ -139,20 +144,9 @@ window.onload = function () {
         //     }
         //     gridDiv.appendChild(newRow);
         // }
-
-        for (var x = 0; x < SIZE; x++) {
-            myGrid[x] = [];
-
-            for (var y = 0; y < SIZE; y++) {
-                myGrid[x][y] = new Cell(x, y);
-
-            }
-        }
-
     }
 
 }
-
 // function work(){
 //  make me a maze, a sandwitch, and do my laundry
 // } alert("something cool");
