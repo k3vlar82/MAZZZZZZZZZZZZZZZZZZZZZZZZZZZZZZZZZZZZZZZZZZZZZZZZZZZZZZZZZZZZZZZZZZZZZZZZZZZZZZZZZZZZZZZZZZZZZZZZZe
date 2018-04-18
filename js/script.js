d70afe49ebe22e70dd0142a2791bcc43bbf114ad
Glutamein <36281832+Glutamein@users.noreply.github.com>
2018-04-18 12:15:58 -0600

    xccvbmn,m

--- js/script.js
+++ js/script.js
@@ -1,13 +1,9 @@
 window.onload = function () {
 
-    // var theCanvas=document.getElementById("myCanvas");
-    // var theContext=theCanvas.getContext("2d");
-
-    // var c=document.getElementById("myCanvas");
-    // var ctx=c.getContext("2d");
-
-    const SIZE = 6;
+    const SIZE = 3;
     const CELL_SIZE = 69;
+
+//canvas size = cell-size*size
 
     var myGrid = [];
 
@@ -18,8 +14,15 @@ window.onload = function () {
         this.visited = false;
         this.neighborsX = [x - 1, x, x + 1, x];
         this.neighborsY = [y, y + 1, y, y - 1];
+
+        this.startDistance;
+
+        this.left;
+        this.right;
+        this.up;
+        this.down;
     }
-    var visited = false;
+    //var visited = false;
     redrawGrid();
 
     console.log(myGrid);
@@ -41,7 +44,6 @@ window.onload = function () {
 
     explore(start.x, start.y, myGrid);
 
-
     function explore(x, y, myGrid) {
         var go = myGrid[start.x][start.y];
         var directions = [0, 1, 2, 3];
@@ -57,37 +59,41 @@ window.onload = function () {
         // if can go down
         for (var i = 0; i < directions.length; i++) {
             //var direction = Math.floor(Math.random() * 4);
-            console.log(directions);
+          //  console.log(directions);
             //  console.log(myGrid[x][y]);
-            console.log(go);
+           // console.log(go);
             if (directions[i] == 0 && x - 1 >= 0) {
-                var go = myGrid[x - 1][y];
+                var go = myGrid[start.x][start.y];
+                 go = myGrid[x - 1][y];
                 left(go.x, go.y);
-                console.log(go)
+                console.log(go);
                 back.push(go);
                 explore(go.x, go.y, myGrid);
-            } else if (directions[i] == 1 && x + 1 < SIZE) {
-                var go = myGrid[x + 1][y];
+            }  if (directions[i] == 1 && x + 1 < SIZE) {
+                var go = myGrid[start.x][start.y];
+                 go = myGrid[x + 1][y];
                 right(go.x, go.y);
-                console.log(go)
+                console.log(go);
                 back.push(go);
                 explore(go.x, go.y, myGrid);
-            } else if (directions[i] == 2 && y + 1 < SIZE) {
-                var go = myGrid[x][y + 1];
+            }  if (directions[i] == 2 && y + 1 < SIZE) {
+                var go = myGrid[start.x][start.y];
+                 go = myGrid[x][y + 1];
                 down(go.x, go.y);
                 console.log(go);
                 back.push(go);
                 explore(go.x, go.y, myGrid);
-            } else if (directions[i] == 3 && y - 1 >= 0) { // y is 0 - goes to this, pops last cell, goes back to this
-                var go = myGrid[x][y - 1];
+            }  if (directions[i] == 3 && y - 1 >= 0) {
+                var go = myGrid[start.x][start.y];
+                 go = myGrid[x][y - 1];
                 up(go.x, go.y);
                 console.log(go);
                 back.push(go);
                 explore(go.x, go.y, myGrid);
             }
-            // else if (go.visited) {
+            // else {
             //     back.pop();
-            //     explore(cell.x, cell.y, myGrid);
+            //     explore(go.x, go.y, myGrid);
             // }
         }
     }
@@ -111,8 +117,6 @@ window.onload = function () {
 
             }
         }
-
-
     }
 
     function left(x, y) {
@@ -122,8 +126,6 @@ window.onload = function () {
         go.visited = true;
         back.push(go);
         // }
-
-
     }
 
     function right(x, y) {
@@ -148,10 +150,6 @@ window.onload = function () {
         // if (!go.visited) {
         go.visited = true;
         back.push(go);
-        // }
     }
 
 }
-// function work(){
-//  make me a maze, a sandwitch, and do my laundry
-// } alert("something cool");
